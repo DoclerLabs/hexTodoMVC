2fd9a76f95c4512ceae9eea6bf1e30e3ba117e02
francis.bourre <francis_bourre@me.com>
2016-08-18 13:57:32 +0200

    Short lambda used for Array selection

--- src/todomvc/model/TodoModel.hx
+++ src/todomvc/model/TodoModel.hx
@@ -4,7 +4,6 @@ import common.ITodoConnection;
 import common.TodoItem;
 import hex.di.IInjectorContainer;
 import hex.mdvc.model.IOutput;
-import hex.util.LambdaMacro;
 
 using hex.util.ArrayUtil;
 
@@ -29,63 +28,61 @@ class TodoModel implements ITodoModel implements IInjectorContainer
 	
 	@Debug public function getActiveItems() : Array<TodoItem>
 	{
-		return this._items.filter( function( e ) { return !e.completed; } );
+		return this._items.findAll( e => !e.completed );
 	}
 	
 	@Debug public function getCompletedItems() : Array<TodoItem>
 	{
-		return this._items.filter( function( e ) { return e.completed; } );
+		return this._items.findAll( e => e.completed );
 	}
 	
 	@Debug public function addItem( item : TodoItem ) : Void
 	{
 		this.output.onClearNewTodo();
 		this._items.push( item );
-		this.output.onShowEntries( this._items );
+		this.output.onShowEntries( this._items.copy() );
 		this._updateCount();
 	}
 	
 	@Debug public function removeItem( id : String ) : Void
 	{
-		this._items.doWhen( function( e ) { return e.id == id; },
-			function( e, i ) { this._items.splice( i, 1 ); this.output.onRemoveItem( e.id ); this._updateCount(); } );
-			
-		//this._items.doWhen( ( e => e.id == id ), function( e, i ) { this._items.splice( i, 1 ); this.output.onRemoveItem( e.id ); this._updateCount(); } );
+		this._items.findAll( e => e.id == id )
+			.forEach( e => function( e ){ this._items.remove( e ); this.output.onRemoveItem( e.id ); this._updateCount(); } );
 	}
 	
 	@Debug public function startItemEdition( id : String ) : Void
 	{
-		var item = this._items.findElement( function( e ){ return e.id == id;  } );
+		var item = this._items.find( e => e.id == id );
 		this.output.onEditItem( item.id, item.title );
 	}
 	
 	@Debug public function removeCompletedItems() : Void
 	{
-		this._items.doWhen( function( e ) { return e.completed == true; },
-			function( e, i ) { this.output.onRemoveItem( e.id ); this._items.splice( i, 1 ); this._updateCount(); });
+		this._items.findAll( e => e.completed == true )
+			.forEach( e => function( e ){ this.output.onRemoveItem( e.id ); this._items.remove( e ); this._updateCount(); } );
 	}
 
 	@Debug public function setItemCompleted( id : String, isCompleted : Bool ) : Void
 	{
-		this._items.doOnFirstWhen( function( e ) { return e.id == id; },
-			function( e, i ) { e.completed = isCompleted; this.output.onSetItemCompleted( id, isCompleted ); this._updateCount(); });
+		this._items.findAll( e => e.id == id )
+			.forEach( e => function( e ){ e.completed = isCompleted; this.output.onSetItemCompleted( id, isCompleted ); this._updateCount(); } );
 	}
 	
 	@Debug public function renameItem( id : String, title : String ) : Void
 	{
-		this._items.doOnFirstWhen( function( e ) { return e.id == id; },
-			function( e, i ) { e.title = title; this.output.onEditItemDone( id, title ); this._updateCount(); });
+		this._items.findAll( e => e.id == id )
+			.forEach( e => function( e ){ e.title = title; this.output.onEditItemDone( id, title ); this._updateCount(); } );
 	}
 	
 	@Debug public function cancelItemEdition ( id : String ) : Void
 	{
-		this._items.doOnFirstWhen( function( e ) { return e.id == id; },
-			function( e, i ) { this.output.onEditItemDone( id, e.title ); this._updateCount(); });
+		this._items.findAll( e => e.id == id )
+			.forEach( e => function( e ){ this.output.onEditItemDone( id, e.title ); this._updateCount(); } );
 	}
 	
 	@Debug public function toggleAllItems( isCompleted : Bool ) : Void
 	{
-		this._items.doOnAll( function( e, i ) { e.completed = isCompleted; this.output.onSetItemCompleted( e.id, e.completed ); });
+		this._items.forEach( e => function( e ){ e.completed = isCompleted; this.output.onSetItemCompleted( e.id, e.completed ); } );
 		this._updateCount();
 	}
 	
